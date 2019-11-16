eb81ae6828832b1848b220d9f6f760c471e1e09b
Troy Van <troygab0@gmail.com>
2019-11-16 12:07:39 -0600

    Initial GLM exploration

---
+++ GLM exploration.R
@@ -0,0 +1,34 @@
+library(pROC)
+
+# ---How to do cross-validation---
+# The quotes have been randomly sorted into 10 groups of equal size. For each group i:
+# - Fit a GLM with observations NOT in group i
+# - Use that model to predict the probabilities of conversion for observations in group i
+# Then, compute the AUC with the generated probabilities of conversion
+glmcv = function(formula) {
+  for(i in 1:10){
+    fitting_data = policies_train[policies_train$group != i,]
+    mod = glm(formula, binomial, fitting_data)
+    policies_train$pred_prob[policies_train$group == i] = predict(mod, policies_train[policies_train$group == i,], type = "response")
+  }
+  
+  # If no predicted probablity, use mean conversion rate as default
+  policies_train$pred_prob[is.na(policies_train$pred_prob)] = mean(policies_train$convert_ind)
+  
+  auc(response = policies_train$convert_ind, predictor = policies_train$pred_prob)
+}
+
+glmcv(convert_ind ~ quoted_amt + discount + Home_policy_ind + Prior_carrier_grp +
+        credit_score + Cov_package_type + CAT_zone + number_drivers + total_number_veh +
+        primary_parking)
+# Cross-Validation AUC = .6337
+
+summary(step(glm(convert_ind ~ quoted_amt + discount + Home_policy_ind + Prior_carrier_grp +
+           credit_score + Cov_package_type + CAT_zone + number_drivers + total_number_veh +
+           primary_parking, binomial, policies_train), trace = 0))
+# Dropped Home_policy_ind, total_number_veh
+
+glmcv(convert_ind ~ quoted_amt + discount + Prior_carrier_grp + 
+        credit_score + Cov_package_type + CAT_zone + number_drivers + 
+        primary_parking)
+# Cross-Validation AUC = .6342
