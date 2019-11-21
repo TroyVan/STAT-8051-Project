e8840e68ee453e1cdf3f22036f7ae10316f071d2
Troy Van <troygab0@gmail.com>
2019-11-20 19:11:04 -0600

    GLM exploration - dealing with NA values, take 2
    Tried to assign defalut values to training set as well; nothing but a waste of time #4

--- GLM exploration - dealing with NA values.R
+++ GLM exploration - dealing with NA values.R
@@ -48,3 +48,58 @@ sum(is.na(test_predictions$conv_prob))
 write.csv(test_predictions, "Troy's GLM exploration test predictions 2019-11-20.csv", row.names = FALSE)
 
 # Test AUC = .61661
+
+# OK, so that's a waste of time. What if we assign default values to the training set as well?
+
+policies_train_noNA = policies_train
+
+policies_train_noNA$quoted_amt[is.na(policies_train_noNA$quoted_amt)] = mean(policies_train$quoted_amt, na.rm = TRUE)
+policies_train_noNA$Prior_carrier_grp[is.na(policies_train_noNA$Prior_carrier_grp)] = mode(policies_train$Prior_carrier_grp)
+policies_train_noNA$Cov_package_type[is.na(policies_train_noNA$Cov_package_type)] = mode(policies_train$Cov_package_type)
+policies_train_noNA$credit_score[is.na(policies_train_noNA$credit_score)] = mean(policies_train$credit_score, na.rm = TRUE)
+policies_train_noNA$CAT_zone[is.na(policies_train_noNA$CAT_zone)] = mode(policies_train$CAT_zone)
+
+# Drop unused predictors
+policies_train_noNA$zip = NULL
+policies_train_noNA$county_name = NULL
+policies_train_noNA$Agent_cd = NULL
+
+# Verify that no NA values remain
+sum(is.na(policies_train_noNA))
+
+# Now redo cross-validation
+
+library(pROC)
+
+# ---How to do cross-validation---
+# The quotes have been randomly sorted into 10 groups of equal size. For each group i:
+# - Fit a GLM with observations NOT in group i
+# - Use that model to predict the probabilities of conversion for observations in group i
+# Then, compute the AUC with the generated probabilities of conversion
+glmcv = function(formula) {
+  for(i in 1:10){
+    fitting_data = policies_train_noNA[policies_train_noNA$group != i,]
+    mod = glm(formula, binomial, fitting_data)
+    policies_train_noNA$pred_prob[policies_train_noNA$group == i] = predict(mod, policies_train_noNA[policies_train_noNA$group == i,], type = "response")
+  }
+  
+  auc(response = policies_train_noNA$convert_ind, predictor = policies_train_noNA$pred_prob)
+}
+
+glmcv(convert_ind ~ discount + Home_policy_ind + state_id + quoted_amt + Prior_carrier_grp +
+        credit_score + Cov_package_type + CAT_zone + number_drivers + num_loaned_veh +
+        num_owned_veh + num_leased_veh + total_number_veh + primary_parking)
+# Cross-Validation AUC = .6356, lower than before
+
+summary(step(glm(convert_ind ~ discount + Home_policy_ind + state_id + quoted_amt + Prior_carrier_grp +
+                   credit_score + Cov_package_type + CAT_zone + number_drivers + num_loaned_veh +
+                   num_owned_veh + num_leased_veh + total_number_veh + primary_parking,
+                 binomial, policies_train_noNA), trace = 0))
+# Dropped Home_policy_ind, num_loaned_veh, num_owned_veh, num_leased_veh, total_number_veh; no change
+
+glmcv(convert_ind ~ discount + state_id + quoted_amt + 
+        Prior_carrier_grp + credit_score + Cov_package_type + CAT_zone + 
+        number_drivers + primary_parking)
+# Cross-Validation AUC = .6364, lower than before
+
+# A bigger waste of time; I won't even bother testing the test set
